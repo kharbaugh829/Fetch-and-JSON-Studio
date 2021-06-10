7b39d2304f17381efa68e2b7861b234ff5b58aed
kharbaugh829 <kharbaugh829@Kaelas-MacBook-Pro.local>
2021-06-09 20:33:57 -0500

    all the way up to bonus mission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json){
+            const container = document.getElementById("container");
+            //something about ording let hoursInSpaceOrder = [];
+            // for (let i =0; i < json.length; i++) {
+            //     hoursInSpace[i] = json[i].hoursInSpace;
+            //     let hours.sort = hoursInSpaceOrder;
+            // };
+            for (let i=0; i < json.length; i++) {
+
+                container.innerHTML += `
+                <div class="astronaut">
+                     <div class="bio">
+                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${json[i].housInSpace}</li>
+                            <li>Active: ${json[i].active}</li>
+                            <li>Skills: ${json[i].skills}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${json[i].picture}">
+                </div>
+
+            `;
+            }
+            
+        });
+
+    });
+    
+    
+});
